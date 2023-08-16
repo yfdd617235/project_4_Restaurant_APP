bbd45e3b77d6b6aabce75a82374f73c95b981963
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2023-08-15 21:08:37 -0500

    update booking

--- src/components/bookings/Bookings.jsx
+++ src/components/bookings/Bookings.jsx
@@ -1,11 +1,40 @@
 
 import React from 'react'
+import { Form } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
 
 function Bookings() {
+
+  function onSave(){
+    let a = 1;
+  }
+
   return (
-    <div>
-        <h1>Bookings</h1>
-    </div>
+    <>
+      <div className='wrap'>
+        <div className='formContent'>
+          <div className='colum'>
+            <Form className='form-reserva' onSubmit={onSave}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Type your full name" />
+                <Form.Label>Cellphone</Form.Label>
+                <Form.Control type="text" placeholder="Type your Cellphone" />
+                <Form.Label>Booking Date</Form.Label>
+                <Form.Control type="date" placeholder="Type your full name" />
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Type your full name" />
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Type your full name" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
