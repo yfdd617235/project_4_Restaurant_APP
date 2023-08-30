e6a9f73ee784d270b9bfe6cc8634ddfafab4142b
yfdd617235 <yosefdavid91@gmail.com>
2023-08-30 16:43:52 -0500

    updae nav bar

--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -6,17 +6,21 @@ import './navigator.css'
 function Navigator() {
     return (
         <>
-        <nav className='navigator' >
-        {/* <div className='container' > */}
-            <ul className='navMenu' >
-                <li><NavLink className="link-menu" to="/"><img className='img-logo' src={logob} alt="grill-img" /></NavLink></li>
-                <li><NavLink className="link-menu" to="/">Home</NavLink></li>
-                <li><NavLink className="link-menu" to="/AboutUs">About Us</NavLink></li>
-                <li><NavLink className="link-menu" to="/Menu">Menu</NavLink></li>
-                <li><NavLink className="link-menu" to="/Bookings">Bookings</NavLink></li>
-            </ul>
-        {/* </div> */}
-        </nav>
+            <div className='nav-container'>
+                <div className='name-container'>
+                    <h2>Texas Grill</h2>
+                    <img className='img-logo' src={logob} alt="grill-img"/>
+                </div>
+                <nav className='navigator' >
+                    <ul className='navMenu' >
+                        <li><NavLink className="link-menu" to="/">Home</NavLink></li>
+                        <li><NavLink className="link-menu" to="/AboutUs">About Us</NavLink></li>
+                        <li><NavLink className="link-menu" to="/Menu">Menu</NavLink></li>
+                        <li><NavLink className="link-menu" to="/Bookings">Bookings</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
+
         </>
     )
 }
