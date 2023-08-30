b5303913284add7494f8ed1f79e90e4a5578f9d7
yfdd617235 <yosefdavid91@gmail.com>
2023-08-30 15:41:34 -0500

    update colors and logo

--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import logob from '../logos/grillsvgw.svg'
 import './navigator.css'
 
 function Navigator() {
@@ -8,6 +9,7 @@ function Navigator() {
         <nav className='navigator' >
         {/* <div className='container' > */}
             <ul className='navMenu' >
+                <li><NavLink className="link-menu" to="/"><img className='img-logo' src={logob} alt="grill-img" /></NavLink></li>
                 <li><NavLink className="link-menu" to="/">Home</NavLink></li>
                 <li><NavLink className="link-menu" to="/AboutUs">About Us</NavLink></li>
                 <li><NavLink className="link-menu" to="/Menu">Menu</NavLink></li>
