06e12bf43cd856ac6e344898bd948fe102d969ca
Yosef David Giraldo Salazar <yosefdavid91@gmail.com>
2023-08-29 19:05:30 -0500

    update products

--- src/components/menu/products.js
+++ src/components/menu/products.js
@@ -1,93 +1,92 @@
 export const products = [
-    {
-      img: "smoked_brisket.jpg",
-      name: "Smoked Brisket",
-      description: "Tender smoked beef brisket seasoned with Texas spices.",
-      price: 18.99
-    },
-    {
-      img: "tex_mex_chili.jpg",
-      name: "Tex-Mex Chili",
-      description: "Hearty chili made with Texas-style spices, ground beef, and beans.",
-      price: 12.49
-    },
-    {
-      img: "chicken_fried_steak.jpg",
-      name: "Chicken Fried Steak",
-      description: "Classic Texas comfort food - battered and fried steak topped with creamy gravy.",
-      price: 15.75
-    },
-    {
-      img: "bbq_ribs.jpg",
-      name: "BBQ Ribs",
-      description: "Succulent pork ribs smoked to perfection and glazed with tangy BBQ sauce.",
-      price: 16.99
-    },
-    {
-      img: "jalapeno_poppers.jpg",
-      name: "Jalapeño Poppers",
-      description: "Spicy jalapeños stuffed with cream cheese, battered, and fried until golden.",
-      price: 8.25
-    },
-    {
-      img: "texas_pulled_pork_sandwich.jpg",
-      name: "Texas Pulled Pork Sandwich",
-      description: "Slow-cooked pulled pork piled high on a bun, served with coleslaw.",
-      price: 13.99
-    },
-    {
-      img: "biscuits_and_gravy.jpg",
-      name: "Biscuits and Gravy",
-      description: "Fluffy biscuits smothered in creamy sausage gravy, a Southern favorite.",
-      price: 10.50
-    },
-    {
-      img: "cowboy_beans.jpg",
-      name: "Cowboy Beans",
-      description: "Hearty baked beans cooked with bacon, ground beef, and barbecue sauce.",
-      price: 7.75
-    },
-    {
-      img: "frito_pie.jpg",
-      name: "Frito Pie",
-      description: "Crispy Fritos topped with chili, cheese, onions, and jalapeños.",
-      price: 9.99
-    },
-    {
-      img: "texas_cornbread.jpg",
-      name: "Texas Cornbread",
-      description: "Moist and slightly sweet cornbread, a classic side dish in Texas.",
-      price: 5.50
-    },
-    {
-      img: "grilled_sausage.jpg",
-      name: "Grilled Sausage",
-      description: "Juicy sausage links with a smoky flavor, perfect for BBQ lovers.",
-      price: 11.25
-    },
-    {
-      img: "pulled_chicken.jpg",
-      name: "Pulled Chicken",
-      description: "Tender pulled chicken in a tangy Texas-style BBQ sauce.",
-      price: 14.75
-    },
-    {
-      img: "loaded_potato_skins.jpg",
-      name: "Loaded Potato Skins",
-      description: "Crispy potato skins filled with cheese, bacon, and green onions.",
-      price: 9.50
-    },
-    {
-      img: "grilled_corn.jpg",
-      name: "Grilled Corn",
-      description: "Corn on the cob grilled to perfection and brushed with savory butter.",
-      price: 4.99
-    },
-    {
-      img: "pecan_pie.jpg",
-      name: "Pecan Pie",
-      description: "Rich and gooey pecan pie, a classic Southern dessert.",
-      price: 6.99
-    }
-  ];
-  
+  {
+    img: "./imgs/img1.jpeg",
+    name: "Smoked Brisket",
+    description: "Tender smoked beef brisket seasoned with Texas spices.",
+    price: 18.99
+  },
+  {
+    img: "./imgs/img2.jpeg",
+    name: "Tex-Mex Chili",
+    description: "Hearty chili made with Texas-style spices, ground beef, and beans.",
+    price: 12.49
+  },
+  {
+    img: "./imgs/img3.jpeg",
+    name: "Chicken Fried Steak",
+    description: "Classic Texas comfort food - battered and fried steak topped with creamy gravy.",
+    price: 15.75
+  },
+  {
+    img: "./imgs/img4.jpeg",
+    name: "BBQ Ribs",
+    description: "Succulent pork ribs smoked to perfection and glazed with tangy BBQ sauce.",
+    price: 16.99
+  },
+  {
+    img: "./imgs/img5.jpeg",
+    name: "Jalapeño Poppers",
+    description: "Spicy jalapeños stuffed with cream cheese, battered, and fried until golden.",
+    price: 8.25
+  },
+  {
+    img: "./imgs/img6.jpg",
+    name: "Texas Pulled Pork Sandwich",
+    description: "Slow-cooked pulled pork piled high on a bun, served with coleslaw.",
+    price: 13.99
+  },
+  {
+    img: "./imgs/img7.jpg",
+    name: "Biscuits and Gravy",
+    description: "Fluffy biscuits smothered in creamy sausage gravy, a Southern favorite.",
+    price: 10.50
+  },
+  {
+    img: "./imgs/img8.jpg",
+    name: "Cowboy Beans",
+    description: "Hearty baked beans cooked with bacon, ground beef, and barbecue sauce.",
+    price: 7.75
+  },
+  {
+    img: "./imgs/img9.jpg",
+    name: "Frito Pie",
+    description: "Crispy Fritos topped with chili, cheese, onions, and jalapeños.",
+    price: 9.99
+  },
+  {
+    img: "./imgs/img10.jpg",
+    name: "Texas Cornbread",
+    description: "Moist and slightly sweet cornbread, a classic side dish in Texas.",
+    price: 5.50
+  },
+  {
+    img: "./imgs/img11.jpg",
+    name: "Grilled Sausage",
+    description: "Juicy sausage links with a smoky flavor, perfect for BBQ lovers.",
+    price: 11.25
+  },
+  {
+    img: "./imgs/img12.jpg",
+    name: "Pulled Chicken",
+    description: "Tender pulled chicken in a tangy Texas-style BBQ sauce.",
+    price: 14.75
+  },
+  {
+    img: "./imgs/img13.jpg",
+    name: "Loaded Potato Skins",
+    description: "Crispy potato skins filled with cheese, bacon, and green onions.",
+    price: 9.50
+  },
+  {
+    img: "./imgs/img14.jpg",
+    name: "Grilled Corn",
+    description: "Corn on the cob grilled to perfection and brushed with savory butter.",
+    price: 4.99
+  },
+  {
+    img: "./imgs/img15.jpg",
+    name: "Pecan Pie",
+    description: "Rich and gooey pecan pie, a classic Southern dessert.",
+    price: 6.99
+  }
+];
