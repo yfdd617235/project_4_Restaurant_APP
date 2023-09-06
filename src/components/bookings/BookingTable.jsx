8da9fdc46d61ebf4c437dd1564b9a46659bc05fb
yfdd617235 <yosefdavid91@gmail.com>
2023-09-06 12:48:37 -0500

    separate booking components , css to booking table

--- src/components/bookings/BookingTable.jsx
+++ src/components/bookings/BookingTable.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Table from 'react-bootstrap/Table'
+import bookingtable from './bookingtable.css'
+import { collection, onSnapshot } from 'firebase/firestore'
+import db from '../../services/firebase.js'
 
 function BookingTable() {
+  const [bookings, setBookings] = useState([]);
+
+  async function showBookings() {
+    onSnapshot(collection(db, 'booking'), (snapshot) => {
+      const list = [];
+      snapshot.forEach(element => {
+        list.push({ ...element.data(), id: element.id })
+        setBookings(list)
+      });
+    })
+    console.log("Bookings saved", bookings)
+  }
+
+  useEffect(() => {
+    showBookings();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <>BookingTable</>
+    <>
+      <div className='table-container'>
+        <h4>Bookings Table</h4><br />
+        <Table className='table-scroll'> 
+          <thead>
+            <tr>
+              <th>Full Name</th>
+              <th>Cellphone</th>
+              <th>Date</th>
+              <th>Hour</th>
+              <th>Diners</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              bookings.map((booking, index) => {
+                return (
+                  <tr key={index}> {/*this key is to solve warning of react identification ID*/}
+                    <td>{booking.name}</td>
+                    <td>{booking.cell}</td>
+                    <td>{booking.date}</td>
+                    <td>{booking.hour}</td>
+                    <td>{booking.diners}</td>
+                    <td>{booking.email}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </Table>
+      </div>
+    </>
   )
 }
 
